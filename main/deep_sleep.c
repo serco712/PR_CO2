3c58e1237af1a9042b1f95aa2d890d27dce7cd4d
emifh7 <emiliano.fh@exatec.tec.mx>
2024-12-16 19:32:28 +0100

    Create deep_sleep.c

---
+++ main/deep_sleep.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "esp_sleep.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "sdkconfig.h"
+
+
+#if SOC_RTC_FAST_MEM_SUPPORTED
+static RTC_DATA_ATTR struct timeval sleep_enter_time;
+#else
+static struct timeval sleep_enter_time;
+#endif
+
+static void deep_sleep_task(void)
+{
+    /**
+     * Prefer to use RTC mem instead of NVS to save the deep sleep enter time, unless the chip
+     * does not support RTC mem(such as esp32c2). Because the time overhead of NVS will cause
+     * the recorded deep sleep enter time to be not very accurate.
+     */
+#if !SOC_RTC_FAST_MEM_SUPPORTED
+    // Initialize NVS
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    nvs_handle_t nvs_handle;
+    err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
+    if (err != ESP_OK) {
+        printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+    } else {
+        printf("Open NVS done\n");
+    }
+
+    // Get deep sleep enter time
+    nvs_get_i32(nvs_handle, "slp_enter_sec", (int32_t *)&sleep_enter_time.tv_sec);
+    nvs_get_i32(nvs_handle, "slp_enter_usec", (int32_t *)&sleep_enter_time.tv_usec);
+#endif
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+#if CONFIG_IDF_TARGET_ESP32
+    // Isolate GPIO12 pin from external circuits. This is needed for modules
+    // which have an external pull-up resistor on GPIO12 (such as ESP32-WROVER)
+    // to minimize current consumption.
+    rtc_gpio_isolate(GPIO_NUM_12);
+#endif
+
+    
+    printf("Entering deep sleep\n");
+    // get deep sleep enter time
+    gettimeofday(&sleep_enter_time, NULL);
+
+#if !SOC_RTC_FAST_MEM_SUPPORTED
+    // record deep sleep enter time via nvs
+    ESP_ERROR_CHECK(nvs_set_i32(nvs_handle, "slp_enter_sec", sleep_enter_time.tv_sec));
+    ESP_ERROR_CHECK(nvs_set_i32(nvs_handle, "slp_enter_usec", sleep_enter_time.tv_usec));
+    ESP_ERROR_CHECK(nvs_commit(nvs_handle));
+    nvs_close(nvs_handle);
+#endif
+
+    // enter deep sleep
+    //example_deep_sleep_register_rtc_timer_wakeup();
+    printf("Preparándose para entrar en deep sleep...\n");
+    esp_deep_sleep_start();
+}
+
+static void example_deep_sleep_register_rtc_timer_wakeup(void)
+{
+    const int wakeup_time_sec = 10;
+    printf("Enabling timer wakeup, %ds\n", wakeup_time_sec);
+    ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000));
+}
