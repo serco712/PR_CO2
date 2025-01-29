a16c13e5a79c11dbd3cdccd04d400ebecc1c9b60
emifh7 <emiliano.fh@exatec.tec.mx>
2025-01-29 12:27:56 +0100

    Update deep_sleep.c

--- main/deep_sleep.c
+++ main/deep_sleep.c
@@ -5,6 +5,7 @@
 #include "freertos/task.h"
 #include "driver/uart.h"
 #include "esp_sleep.h"
+#include "esp_pm.h"
 #include "esp_log.h"
 #include "esp_timer.h"
 #include "nvs_flash.h"
@@ -17,6 +18,14 @@ static RTC_DATA_ATTR struct timeval sleep_enter_time;
 #else
 static struct timeval sleep_enter_time;
 #endif
+
+esp_pm_config_t pow_mng = {
+        .max_freq_mhz = 160,
+        .min_freq_mhz = 40,
+        .light_sleep_enable = true
+    };
+
+    esp_pm_configure(&pow_mng);
 
 static void deep_sleep_task(void)
 {
